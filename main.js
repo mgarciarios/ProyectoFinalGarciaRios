99e5ccb55f924f9d47350ee387338390e042db85
mgarciarios <martinamgarciarios@gmail.com>
2023-06-07 12:48:54 -0300

    Segunda pre-entrega

--- main.js
+++ main.js
@@ -1,68 +1,58 @@
-const recargoFijo = 1.92;
-const cordoba = 20000;
-const bariloche = 25000;
-const salta = 40000;
-const santaCruz = 30000;
-const tucuman = 35000;
-
-function calculadoraDelCosto(ingresoDestino, cantidadDePersonas, recargoFijo, ingresoCuotas) {
-    return (((ingresoDestino * cantidadDePersonas) * recargoFijo) / ingresoCuotas);
-}
-
-function opcionesDestino(ingresoDestino, cantidadDeVuelos) {
-    let montoTotal 
-    switch (parseInt(ingresoDestino)) {
-        case 1:
-            montoTotal = calculadoraDelCosto(cordoba * cantidadDeVuelos, cantidadDePersonas, recargoFijo, ingresoCuotas);
-            break;
-
-        case 2:
-            montoTotal = calculadoraDelCosto(bariloche * cantidadDeVuelos, cantidadDePersonas, recargoFijo, ingresoCuotas);
-            break;
-
-        case 3:
-            montoTotal = calculadoraDelCosto(salta * cantidadDeVuelos, cantidadDePersonas, recargoFijo, ingresoCuotas);
-            break;
-
-        case 4:
-            montoTotal = calculadoraDelCosto(santaCruz * cantidadDeVuelos, cantidadDePersonas, recargoFijo, ingresoCuotas);
-            break;
-
-        case 5:
-            montoTotal = calculadoraDelCosto(tucuman * cantidadDeVuelos, cantidadDePersonas, recargoFijo, ingresoCuotas);
-            break;
-    
-        default:
-            alert("No se ha comprendido el valor ingresado.");
+class Destino {
+    constructor(codigo, lugar, precio) {
+        this.codigo = parseInt(codigo);
+        this.lugar = lugar;
+        this.precio = parseFloat(precio);
     }
-    return montoTotal;
+    precioConRecargo() {
+        this.precio = this.precio * 1.92;
+    }
+    calcularCosto(cantidadDePersonas, cantidadDeVuelos, ingresarCuotas){
+        this.precioConRecargo();
+        return ((this.precio * cantidadDePersonas * cantidadDeVuelos) / ingresarCuotas); 
+    }
 }
 
-let ingresoNombre = prompt("¿Como es tu nombre completo?");
+let cordoba = new Destino(1, "cordoba", 20000);
+let bariloche = new Destino(2, "bariloche", 25000);
+let salta = new Destino(3, "salta", 40000);
+let santaCruz = new Destino(4, "santaCruz", 30000);;
+let tucuman = new Destino(5, "tucuman", 35000)
+
+const destinosDisponibles = [cordoba, bariloche, salta, santaCruz, tucuman];
+
+let destinoSeleccionado = {} 
+
+function corroborarDestino() {
+    destinoSeleccionado = destinosDisponibles.find((element) => element.codigo === parseInt(codigo));
+    if (destinoSeleccionado === undefined){
+        alert(ingresarNombre + ", NO SE ENCONTRÓ EL DESTINO INDICADO.");
+        return;
+    } else{
+        let montoFinal = destinoSeleccionado.calcularCosto(cantidadDePersonas, cantidadDeVuelos, ingresarCuotas) 
+        alert(ingresarNombre + " DEBERÁ PAGAR " + ingresarCuotas + " CUOTAS DE $ " + montoFinal + " CADA UNA.")
+    }
+}
+
+let ingresarNombre = prompt("¿Como es tu nombre completo?").toUpperCase();
 let cantidadDePersonas = parseInt(prompt("¿Cual es el número de pasajeros que viajan?"));
-let ingresoCuotas = parseInt(prompt("¿En cuantas cuotas desearía sacar el vuelo? Se puede realizar hasta 36 cuotas"));
-if ((ingresoCuotas>=1) && (ingresoCuotas<=36)){
+let ingresarCuotas = parseInt(prompt("¿En cuantas cuotas desearía sacar el vuelo? Se puede realizar hasta 36 cuotas"));
+if ((ingresarCuotas>=1) && (ingresarCuotas<=36)){
     let idaVuelta = confirm("¿Desea sacar pasajes para la ida y vuelta?");
-    if (idaVuelta) {
-        let ingresoDestino = prompt("¿Cuál el código del destino al que desea viajar: (1) Ciudad de Córdoba, (2) San Carlos de Bariloche, (3) Salta, (4) Santa Cruz, (5) Tucumán?");
-        let cantidadDeVuelos = 2;
-        let montoFinal = (opcionesDestino(ingresoDestino, cantidadDeVuelos));
-        for (let i=0; i<ingresoCuotas; i++) {
-             alert(ingresoNombre.trim() + " en la cuota " + (i+1) + " usted pagará: " + montoFinal.toFixed(2));
-    }
-}
-        else if (confirm("¿Desea sacar solo pasaje/s de ida?")) {
-            let ingresoDestino = prompt("¿Cuál es su lugar de destino: Ciudad de Córdoba, San Carlos de Bariloche, Salta, Santa Cruzo Tucumán?");
-            let cantidadDeVuelos = 1;
-            opcionesDestino(ingresoDestino, cantidadDeVuelos);
-            let montoFinal = (opcionesDestino(ingresoDestino, cantidadDeVuelos));
-            for (let i=0; i<ingresoCuotas; i++) {
-                alert(ingresoNombre.trim() + " en la cuota " + (i+1) + " usted pagará: " + montoFinal.toFixed(2));
-       }
-    
-        } else {
-            alert("¡" + ingresoNombre.trim() + " gracias por visitar la página web de FlyArgentina!");
+        if (idaVuelta) {
+            cantidadDeVuelos = 2;
+            codigo = parseInt(prompt("¿Cuál el código del destino al que desea viajar: (1) Ciudad de Córdoba, (2) San Carlos de Bariloche, (3) Salta, (4) Santa Cruz, (5) Tucumán?"));
+            corroborarDestino();
         }
-}else{
-    alert(ingresoNombre.trim() + " has superado el total de cuotas posibles.");
+        else if (confirm("¿Desea sacar solo pasaje/s de ida?")) {
+            cantidadDeVuelos = 1;
+            codigo = parseInt(prompt("¿Cuál el código del destino al que desea viajar: (1) Ciudad de Córdoba, (2) San Carlos de Bariloche, (3) Salta, (4) Santa Cruz, (5) Tucumán?"));
+            corroborarDestino();
+        } 
+        else {
+            alert("¡" + ingresarNombre.trim() + " GRACIAS POR VISITAR LA PÁGINA WEB DE FlyArgentina!");
+        }
+    }
+else{
+    alert(ingresarNombre.trim() + " HAS SUPERADO EL TOTAL DE CUOTAS POSIBLES.");
 }
