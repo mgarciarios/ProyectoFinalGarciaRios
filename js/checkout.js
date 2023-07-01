3cd4fe90e1e06d55efd1be81182c846595cd91c3
mgarciarios <martinamgarciarios@gmail.com>
2023-07-01 18:50:42 -0300

    .

--- js/checkout.js
+++ js/checkout.js
@@ -1,11 +1,17 @@
 const tableBody = document.querySelector('tbody')
 const inputSearch = document.querySelector('input#inputSearch')
 
-const armarFilaDestinos = (destino)=> {
+const armarFilaDestinos = (carritoDestinos)=> {
     return `<tr>
-                <td class="class-table-number">${destino.codigo}</td>
-                <td>${destino.paquete}</td>
-                <td>$ ${destino.importe}</td>
-                <td><button id="${destino.codigo}" class="button button-outline button-big-emoji">🤍</button></td>
+                <td class="class-table-number">${carritoDestinos.codigo}</td>
+                <td>${carritoDestinos.paquete}</td>
+                <td>$ ${carritoDestinos.importe}</td>
+                <td><button id="${carritoDestinos.codigo}" class="button button-outline button-big-emoji">🤍</button></td>
             </tr>`
+}
+
+if (carritoDestinos.length > 0) {
+    carritoDestinos.forEach(carritoDestinos => {
+        tableBody.innerHTML += armarFilaDestinos(carritoDestinos)
+    });
 }
